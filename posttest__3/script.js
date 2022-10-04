62077b0379111bd283888749b36d439787344e1c
AndriNL212 <93073015+AndriNL212@users.noreply.github.com>
2022-10-04 20:41:55 +0800

    Add files via upload

---
+++ posttest__3/script.js
@@ -0,0 +1,10 @@
+const produk = document.getElementById('produk');
+
+produk.addEventListener("click",
+    function(){
+        alert("link gak bisa dibuka >_<");
+    },
+)
+
+const text = document.getElementById("text");
+text.style.color= "#FDFF00";
